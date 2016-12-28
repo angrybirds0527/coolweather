a91c8b1a2394aa8ae060c1c6de716a3f1eeb151f
byx <angrybirds0527@163.com>
2016-12-28 14:04:32 +0800

    add dbconfig and table class

---
+++ app/src/main/java/com/example/coolweather/db/Provider.java
@@ -0,0 +1,40 @@
+package com.example.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by BYX on 2016-12-28.
+ */
+
+public class Provider extends DataSupport {
+    private int id;
+    private String provinceName;
+    private int provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+
+
+
+}
