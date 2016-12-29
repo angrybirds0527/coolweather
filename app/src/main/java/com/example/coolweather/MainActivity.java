8cde09e8a37f7e3e638016520cbd675f76b8d967
byx <angrybirds0527@163.com>
2016-12-29 10:42:03 +0800

    2 commit

--- app/src/main/java/com/example/coolweather/MainActivity.java
+++ app/src/main/java/com/example/coolweather/MainActivity.java
@@ -18,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
             Intent intent = new Intent(this, WeatherActivity.class);
             startActivity(intent);
             finish();
+            //ces
         }
     }
 
